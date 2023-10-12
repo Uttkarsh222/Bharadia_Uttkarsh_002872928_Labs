abea5f87e1198de8870d2bd580916255b60ebbd1
Uttkarsh222 <bharadia.u@northeastern.edu>
2023-10-12 14:48:46 -0400

    lab_3

---
+++ Lab_03/src/model/Account.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author uttkarsh
+ */
+public class Account {
+    private String routingNumber;
+    private String accountNumber;
+    private String bankName;
+    private int balance;
+    private Date createdon;
+
+    public Account() {
+        this.createdon = new Date();
+    }
+    
+    
+
+    public String getRoutingNumber() {
+        return routingNumber;
+    }
+
+    public void setRoutingNumber(String routingNumber) {
+        this.routingNumber = routingNumber;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public Date getCreatedon() {
+        return createdon;
+    }
+    @Override
+    public String toString(){
+        return bankName;
+    }
+}
